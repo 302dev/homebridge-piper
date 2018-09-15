a3ae9bb0ca19e53c222396472042c52bd34a2b06
Brian French <fr3nch13@gmail.com>
2018-09-14 20:11:05 -0700

    Initial commit with the fork. It's not really a fork since I can't fine the original developer, nor find the source repo.

---
+++ README.md
@@ -0,0 +1,47 @@
+#Piper homebridge plugin
+This is a [homebridge](https://github.com/nfarina/homebridge) plugin for the [Piper](https://getpiper.com) security system.
+As currently there is no public API available, this plugin works in combination with [IFTTT](https://ifttt.com) rules.
+
+##Example config.json
+```
+{
+    ...
+    "platforms": [
+        {
+            "platform": "Piper",
+            "serverport": <server_port>,
+            "makerkey": "<your_maker_key>",
+            "pitoken": "<your_pi_token>",
+            "devices": [
+              {
+                "type": "SecuritySystem",
+                "id": "security",
+                "name": "Alarmsysteem"
+              }
+            ]
+        }
+    ]
+    ...
+}
+```
+
+##Configuration requirements
+The following configurations need to be performed for the homebridge-piper plugin to run:
+
+1. Install and configure homebridge on your Raspberry Pi as described [here](https://github.com/nfarina/homebridge/wiki/Running-HomeBridge-on-a-Raspberry-Pi)
+2. Install the homebridge-piper plugin through npm ```npm install -g homebridge-piper```
+3. Setup, for each ```<security_mode>``` 0-3, a rule on IFTTT using the [Maker channel](https://ifttt.com/maker) and [Piper channel](https://ifttt.com/piper) to be able to change the piper security state through a web request:
+  - IF Maker receive web request
+    - Event name ```piperupdate_security_<security_mode>```
+  - THEN Piper change security mode
+  - Security mode 0: Stay, 1: Away, 2: Stay, 3: Disarmed (2x stay because homekit has 4 security states while piper has 3. Homekit night and stay mode both map to piper stay mode)
+4. Setup, for each ```<security_mode>``` 0-3, a rule on IFTTT using the [Maker channel](https://ifttt.com/maker) to provide a callback to the Raspberry Pi that the status has changed. Ideal would be to trigger this event when the piper mode has changed, but this is not provided by the Piper IFTTT channel. Therefore, on an incoming maker web request, we both change the piper status and do the callback. At least then we know the IFTTT rule matched. When the Piper channel will introduce a trigger on security mode change, the if condition of this rule can be changed.
+  - IF Maker receive web request
+    - Event name ```piperupdate_security_<security_mode>```
+  - THEN Maker make web request
+    - Web request url ```http://<your_wan_ip_address>:<server_port>/<your_pi_token>```. Where ```<server_port>``` and ```<your_pi_token>``` match the values provided in the config file. This 'secret' pi token provides a level of security to avoid unappropriate access to the security state.
+    - Method ```POST```
+    - Content type ```application/json```
+    - Body ```{"id": "{{Value1}}", "state": "{{Value2}}"}```
+3. Configure you ```config.json``` file with your maker key (provided by IFTTT when connecting to the Maker channel) and pi token chosen above.
+4. Make sure that your Raspberry Pi is reachable on the provided port number. This might require port forwarding on your router.
---
+++ index.js
@@ -0,0 +1,132 @@
+var PiperContactSensor = require('./lib/contactsensor.js');
+var PiperSecuritySystem = require('./lib/securitysystem.js');
+var PiperMotionSensor = require('./lib/motionsensor.js');
+var http = require('http');
+
+var Characteristic, Service;
+
+module.exports = function (homebridge) {
+	Service = homebridge.hap.Service;
+	Characteristic = homebridge.hap.Characteristic;
+
+	homebridge.registerPlatform("homebridge-piper", "Piper", PiperPlatform);
+};
+
+function PiperPlatform(log, config) {
+	this.log = log;
+	this.config = config;
+	this.Service = Service;
+	this.Characteristic = Characteristic;
+
+	this.makerkey = this.config.makerkey;
+	this.pitoken = this.config.pitoken;
+	this.serverport = parseInt(this.config.serverport);
+
+	//Initialize accessories
+	this._initializeAccessories(this.config.devices);
+
+	//Install HTTP server
+	this.requestServer = http.createServer(this._handleRequest.bind(this));
+	this.requestServer.listen(this.serverport, function() {
+		this.log("Server listening on port " +this.serverport+ "...");
+	}.bind(this));
+};
+
+PiperPlatform.prototype.accessories = function(callback) {
+	this.log("Retrieving accessories for Piper");
+	var accessories = [];
+	for (var id in this.myaccessories) {
+		accessories.push(this.myaccessories[id]);
+	}
+	callback(accessories);
+};
+
+PiperPlatform.prototype._handleRequest = function(request, response) {
+	if(request.url == "/" + this.pitoken) {
+		this.log("Valid web request received");
+		if(request.method == 'POST') {
+			var queryData = "";
+			request.on('data', function(data) {
+				queryData += data;
+				if(queryData.length > 1e6) {
+					queryData = "";
+					response.writeHead(413, {'Content-Type': 'text/plain'});
+					response.end();
+					request.connection.destroy();
+				}
+			});
+			request.on('end', function() {
+			    	//Get the querydat, parse id and call _processStatusUpdate
+				response.writeHead(204, {'Content-Type': 'text/plain'});
+				response.end();
+
+				try{
+					var jso = JSON.parse(queryData);
+					if(jso.id != undefined && jso.state != undefined){
+						this._handleStateChange(jso.id, jso.state);
+					}
+				}catch(err){
+					this.log("Invalid JSON input: " + err);
+				}
+			}.bind(this));
+		} else {
+			response.writeHead(405, {'Content-Type': 'text/plain'});
+			response.end();
+		}
+	}else{
+		this.log("Received invalid url " + request.url + ", returning 403");
+		response.writeHead(403, {'Content-Type': 'text/plain'});
+		response.end();
+	}
+};
+
+PiperPlatform.prototype._handleStateChange = function(id, state) {
+	if(id in this.myaccessories){
+		this.myaccessories[id].processStateChange(state);
+	}
+}
+
+PiperPlatform.prototype._initializeAccessories = function(devices) {
+	this.log("Initializing piper accessories");
+	this.myaccessories = {};
+	this.states = {};
+	for (var id in devices) {
+		if (devices.hasOwnProperty(id)) {
+  			var aconfig = devices[id];
+			var acc = undefined;
+			switch(aconfig.type){
+				case "SecuritySystem":
+					acc = new PiperSecuritySystem(this.log, aconfig, this);
+					break;
+				case "ContactSensor":
+					acc = new PiperContactSensor(this.log, aconfig, this);
+					break;
+				case "MotionSensor":
+					acc = new PiperMotionSensor(this.log, aconfig, this);
+					break;
+				default:
+					this.log("Accessory has invalid type '" + aconfig.type + "' and will be ignored");
+			}
+			if(acc != undefined){
+				this.myaccessories[acc.id] = acc;
+				this.setKnownState(acc.id, acc.getDefaultState());
+			}
+		}
+	}
+};
+
+PiperPlatform.prototype.setKnownState = function(id, state) {
+	if(id in this.myaccessories){
+		this.states[id] = state;
+	}else{
+		this.log("WARNING: Tried to set state of unknown accessory");
+	}
+};
+
+PiperPlatform.prototype.getKnownState = function(id) {
+	if(id in this.states) {
+		return this.states[id];
+	}else{
+		return undefined;
+	}
+};
